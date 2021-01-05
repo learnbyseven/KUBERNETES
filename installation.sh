cbb7d3f61f94cbbf21d8dbf294424b2e0c1c117a
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2021-01-05 14:14:14 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,11 +2,11 @@
 ## curl -s https://raw.githubusercontent.com/learnbyseven/KUBERNETES-TRAINING/master/installation.sh | bash -s 
 
 #!/bin/bash
-sudo echo "Kubernetes vanilla installation begins using KubeADM"
-sudo apt-get update -y 
-sudo apt-get install -y apt-transport-https curl docker.io
+echo "Kubernetes vanilla installation begins using KubeADM"
+apt-get update -y 
+apt-get install -y apt-transport-https curl docker.io
 sleep 1
-sudo cat > /etc/docker/daemon.json <<EOF
+cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -17,24 +17,24 @@ sudo cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 sleep 1 
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo systemctl enable docker
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
 sleep 1 
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sleep 1 
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sleep 1 
-sudo apt-get update
+apt-get update
 echo "KUBERNETES DEFAULT PACKAGE INSTALLATION BEGINS"
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo swapoff -a
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-bind-port=6443 > /kub.txt
-sudo mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo kubectl taint nodes --all node-role.kubernetes.io/master-
-sudo kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+apt-get install -y kubelet kubeadm kubectl
+swapoff -a
+kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-bind-port=6443 > /kub.txt
+mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
 sleep 1
 echo "COMPLETED"
 #### FINISH 
