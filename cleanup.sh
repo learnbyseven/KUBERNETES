72c80bc6fc2408043e968c7dd94177257724a4a0
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2021-01-05 15:03:33 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,5 @@
 ## USE 
 ## curl -s https://raw.githubusercontent.com/learnbyseven/KUBERNETES-TRAINING/master/cleanup.sh | bash 
-#!/bin/bash
 kubeadm reset
-echo "y"
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 rm -rf ~/.kube/
