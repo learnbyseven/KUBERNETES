97b9d2029471390a3c0b943f2c392a81889f3637
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2021-07-23 14:11:16 +0530

    Update add_node_k8.sh

--- add_node_k8.sh
+++ add_node_k8.sh
@@ -14,7 +14,7 @@ cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sudo sysctl --system
+sudo sysctl -p
 sleep 2
 apt-get update -y 
 sleep 1
