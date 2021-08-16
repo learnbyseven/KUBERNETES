7fc3c1abd1046a35b37927239aeb7e81ead3a691
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2021-08-16 10:36:01 +0530

    Update install-K8s.sh

--- install-K8s.sh
+++ install-K8s.sh
@@ -46,12 +46,12 @@ apt-get update
 ##Workaround to disable swap on Linux host 
 #sed -i -e '2s/^/#/' /etc/fstab
 echo "KUBERNETES DEFAULT PACKAGE INSTALLATION BEGINS"
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet=1.20.1-00 kubeadm=1.20.1-00 kubectl=1.20.1-00
 swapoff -a
 kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-bind-port=6443 > /kub.txt
 mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
 sleep 1
 echo "COMPLETED"
 #### FINISH 
