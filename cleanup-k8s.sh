12bdea4e768fdc69fc7cc86e95c71fcd3068d60c
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2022-05-20 17:06:54 +1000

    Update cleanup-k8s.sh

--- cleanup-k8s.sh
+++ cleanup-k8s.sh
@@ -5,5 +5,5 @@ iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 rm -rf ~/.kube/
 rm -rf /etc/kubernetes
 rm -rf /var/lib/kubelet
-apt-get remove --purge kubelet kubeadm kubectl
+apt-get remove --purge kubelet kubeadm kubectl -y
 
