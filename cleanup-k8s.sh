bb84b0d6b61d30502bc98d7fc70a665ccbfeaad6
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2022-05-25 17:35:03 +1000

    Update cleanup-k8s.sh

--- cleanup-k8s.sh
+++ cleanup-k8s.sh
@@ -1,6 +1,6 @@
 ## USE 
 ## curl -s https://raw.githubusercontent.com/learnbyseven/KUBERNETES-TRAINING/master/cleanup-k8s.sh | bash 
-kubeadm reset
+kubeadm reset -y
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 rm -rf ~/.kube/
 rm -rf /etc/kubernetes
