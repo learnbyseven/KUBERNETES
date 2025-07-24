a7d21c36caf55ec9c69bf04eb7f6f1cdc5e9f144
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2025-07-24 15:28:05 +1000

    Update install-K8s.sh

--- Installation/install-K8s.sh
+++ Installation/install-K8s.sh
@@ -3,65 +3,55 @@
 
 #!/bin/bash
 echo "Kubernetes vanilla installation begins using KubeADM"
-echo "####################################################"
-
-sleep 1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
+echo "#####################################"
+echo "Step 1/3 Pre-installation setup!"
+sleep 2
+KUBERNETES_VERSION=v1.29
+CRIO_VERSION=v1.29
+sleep 2
 apt-get clean
 rm /var/lib/dpkg/lock    
 rm /var/cache/apt/archives/lock
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
 sleep 1
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
+modprobe br_netfilter
+echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sleep 2 
+echo "Adding Kuberenetes repos"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
+    tee /etc/apt/sources.list.d/kubernetes.list
 sleep 2
-apt-get update -y 
-sleep 1
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y libpq-dev apt-transport-https curl docker.io
-sleep 1
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-sleep 1 
-systemctl daemon-reload
-systemctl restart docker
-systemctl enable docker
-sleep 1 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sleep 1 
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-sleep 1 
+echo "Adding Cri-O repos"
+sleep 2
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
+    tee /etc/apt/sources.list.d/cri-o.list
+echo "Both repos are added NOW"
+echo "Step 1/3 Finished Successfully !"
+echo "#####################################"
+echo "Step 2/3 Kubernetes and Container runtime CRI-O SETUP!"
+sleep 2
 apt-get update
-##Workaround to disable swap on Linux host 
-#sed -i -e '2s/^/#/' /etc/fstab
-echo "KUBERNETES DEFAULT PACKAGE INSTALLATION BEGINS"
-echo "##############################################"
+sleep 1
+apt-get install -y software-properties-common curl
 sleep 2
-apt-get install -y kubelet=1.22.1-00 kubeadm=1.22.1-00 kubectl=1.22.1-00
+apt-get install -y cri-o kubelet kubeadm kubectl
 swapoff -a
 sleep 2
-echo "KUBERNTES PACKAGE INSTALLATION FINISH"
+echo "Step 2/3 Finished Successfully !"
 echo "#####################################"
+echo "Step 3/3 KUBERNTES access setup and CNI- Calico installation !"
 sleep 1
 kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-bind-port=6443 
 mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 echo "SDN - CALICO INSTALLATION STARTING NOW"
 echo "######################################"
 sleep 1
@@ -70,8 +60,17 @@ sleep 1
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 #kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
 sleep 1
-echo "K8 INSTALLATION COMPLETED"
+echo "Step 3/3 Finished Successfully !"
 echo "#########################"
+echo "Starting Validation"
+sleep 10
+kubectl get nodes
+sleep 3
+crictl pods
+sleep 2
+echo " Success = Node status "Ready" and All Pods in "Ready" state"
+echo " Validation Done"
+echo "#####FINSIH#####"
 #### FINISH 
 
 ## FOR ADDING NODE
